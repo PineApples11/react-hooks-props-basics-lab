fe021a423d4d6e3a82fd3ef25bf80bc2080f44cd
PineApples11 <kinyanjuiivy1@gmail.com>
2025-01-19 04:43:44 +0300

    props practise

--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,15 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home  name="Liza" city="New York" color ="firebrick" />
+      <About  bio="I made this!" 
+      links=
+      {{
+        github : "https://github.com/liza",
+        linkedin: "https://www.linkedin.com/in/liza/",
+      }}
+      />
+
     </div>
   );
 }
